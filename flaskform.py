3855ed3f90a7a1b8785de968fb1139b2923525cb
Brandon Rose <brandomr@brandon-roses-macbook.local>
2014-10-11 10:11:23 -0400

    added pass through functionality from webform to output page

--- flaskform.py
+++ flaskform.py
@@ -8,19 +8,32 @@ from wtforms import Form, BooleanField, TextField, PasswordField, validators, Te
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-
+WTF_CSRF_ENABLED = True
+app.secret_key = 'you-will-never-guess'
 
 class corpus(Form):
-    text = TextAreaField()
+    corpustext = TextAreaField()
 
 @app.route('/')
 @app.route('/flaskform', methods=['GET', 'POST'])
 def flaskform(name=None):
     form = corpus()
+    if request.method == 'POST':
+    	base_text = request.form['corpustext'] 
+    	manipulated_text = len(base_text)
+    	session['base_text'] = base_text
+    	session['manipulated_text'] = manipulated_text
+    	return redirect(url_for('output', base_text=base_text, manipulated_text=manipulated_text))
     return render_template('flaskform.html', 
                            title='flaskform',
                            form=form)
-                           
+
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+	base_text = session['base_text']
+	manipulated_text = session['manipulated_text']
+	return render_template('output.html', title='flaskform', base_text=base_text, manipulated_text=manipulated_text)
+                         
 app.debug = True            
                            
 if __name__ == '__main__':
